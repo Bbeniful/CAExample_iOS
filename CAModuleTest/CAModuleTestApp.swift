7b66429c36a19d10b9516a2727c128af8420e528
Bbeniful <bbenikeszi@gmail.com>
2025-04-12 17:31:01 +0200

    Initial Commit

---
+++ CAModuleTest/CAModuleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CAModuleTestApp.swift
+//  CAModuleTest
+//
+//  Created by Benjámin Szilágyi on 2025. 04. 12..
+//
+
+import SwiftUI
+
+@main
+struct CAModuleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CAModuleTestTests/CAModuleTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  CAModuleTestTests.swift
+//  CAModuleTestTests
+//
+//  Created by Benjámin Szilágyi on 2025. 04. 12..
+//
+
+import Testing
+@testable import CAModuleTest
+
+struct CAModuleTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
